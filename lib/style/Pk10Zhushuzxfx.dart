8e05f35721b9e5490ca7bd1ea6cc29101f7635c7
xigndu <caixingdu@vipyoung.org>
2018-08-25 00:08:32 +0800

    排除-

--- lib/style/Pk10Zhushuzxfx.dart
+++ lib/style/Pk10Zhushuzxfx.dart
@@ -17,7 +17,7 @@ class Pk10Zhushuzxfx {
     int len = arr.length;
     int zhushu = 0;
     switch (len) {
-       case 1:
+      case 1:
         zhushu = r1(arr);
         break;
       case 2:
@@ -42,8 +42,8 @@ class Pk10Zhushuzxfx {
   }
 
   static int r1(List<String> arr) {
-
     Set<String> f1 = split(arr[0], spce).toSet();
+    if(valid(f1)==1) return 0 ;
     return f1.length;
   }
 
@@ -53,7 +53,8 @@ class Pk10Zhushuzxfx {
     int zhushu = 0;
     for (int i = 0; i < f1.length; i++) {
       for (int j = 0; j < f2.length; j++) {
-        if (f1[i] != (f2[j])) {
+        Set<String> set = [f1[i], f2[j]].toSet();
+        if (valid(set)==0 && set.length == 2) {
           zhushu++;
         }
       }
@@ -71,7 +72,7 @@ class Pk10Zhushuzxfx {
         for (int k = 0; k < f3.length; k++) {
 //          print("{},{},{}", f1.get(i), f2.get(j), f3.get(k));
           Set<String> set = [f1[i], f2[j], f3[k]].toSet();
-          if (set.length == 3) {
+          if (valid(set)==0 &&set.length == 3) {
             zhushu = zhushu + 1;
           }
         }
@@ -92,7 +93,7 @@ class Pk10Zhushuzxfx {
           for (int l = 0; l < f4.length; l++) {
 //            log.info("{},{},{},{}", f1.get(i), f2.get(j), f3.get(k), f4.get(l));
             Set<String> set = [f1[i], f2[j], f3[k], f4[l]].toSet();
-            if (set.length == 4) {
+            if (valid(set)==0 &&set.length == 4) {
               zhushu++;
             }
           }
@@ -115,7 +116,7 @@ class Pk10Zhushuzxfx {
           for (int l = 0; l < f4.length; l++) {
             for (int m = 0; m < f5.length; m++) {
               Set<String> set = [f1[i], f2[j], f3[k], f4[l], f5[m]].toSet();
-              if (set.length == 5) {
+              if (valid(set)==0 && set.length == 5) {
                 zhushu++;
               }
             }
@@ -124,5 +125,13 @@ class Pk10Zhushuzxfx {
       }
     }
     return zhushu;
+  }
+
+  static int valid(Set<String> set) {
+    for (var value in set) {
+      if (value == null || value.trim() == '')
+        return 1;
+    }
+    return 0;
   }
 }
