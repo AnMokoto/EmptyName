c8def60deb0d4f88b9a67125fbfbee306520a73d
xigndu <caixingdu@vipyoung.org>
2018-08-25 17:30:17 +0800

    xglhc

--- lib/style/index.dart
+++ lib/style/index.dart
@@ -7,15 +7,16 @@ export 'cqssc.dart';
 import '11x5.dart';
 import 'pk10.dart';
 import 'kl10.dart';
-
+import 'xglhc.dart';
 class StyleSplit {
   StyleSplit._();
 
   /// 返回彩种 实务
   static StyleManagerIMPL of(String str) {
-    if (str.contains("ssc")) if (Style.of(str) != null) return Style.of(str);
+    if (str.contains("ssc")) return Stylessc.of(str) ;
     if (str.contains("11x5")) return Style11x5.of(str) ;
     if (str.contains("pk10")) return Stylepk10.of(str) ;
     if (str.contains("kl10")) return Stylekl10.of(str) ;
+    if (str.contains("lhc")) return Stylexglhc.of(str) ;
   }
 }
