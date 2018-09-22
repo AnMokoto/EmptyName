99b3370024d248478c90e726c99233738e116911
xingdu <caixingdu@vipyoung.org>
2018-09-22 12:59:50 +0800

    pk10开奖号码图片

--- lib/layout/home/opencode/Opencode.dart
+++ lib/layout/home/opencode/Opencode.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:lowlottery/conf/Pk10Color.dart';
 import 'package:lowlottery/conf/XglhcColor.dart';
 import 'package:lowlottery/font/index.dart';
+
 class OpenCode {
   static Container opencode(String gameEn, var _str, int index) {
     if (gameEn == 'pk10') {
@@ -10,7 +11,7 @@ class OpenCode {
     if (gameEn.contains('xglhc')) {
       return _xglhc(_str, index);
     }
-if (gameEn.contains( 'k3')) {
+    if (gameEn.contains('k3')) {
 //      return _k3(_str, index);
     }
 
@@ -66,34 +67,31 @@ if (gameEn.contains( 'k3')) {
     return new Container(
       margin: EdgeInsets.only(left: 4.0),
       decoration: new BoxDecoration(
-        color: Pk10Color.colors(_str[index]),
+//        color: Pk10Color.colors(_str[index]),
         shape: BoxShape.rectangle,
       ),
-      width: 25.0,
-      height: 30.0,
-      child: new Center(
-        child: new Text(
-          _str.length > 1 ? _str[index] : "-",
-          // _str[index] ?? "-",
-          style: const TextStyle(fontSize: 16.0, color: Colors.white),
-          textAlign: TextAlign.center,
-          textDirection: TextDirection.ltr,
-        ),
+      width: 20.0,
+//      height: 30.0,
+      child: new Image.asset(
+        'assets/images/ball-pk${_str[index]}.png',
+        fit: BoxFit.contain,
       ),
     );
-  }static Container _k3(_str, int index) {
-    var icon = AppIcons.shaizi1 ;
-    var va = _str[index] ;
-    if(va == '2'){
-      icon = AppIcons.shaizi2 ;
-    }else if(va == '3'){
-      icon = AppIcons.shaizi3 ;
-    }else if(va == '4'){
-      icon = AppIcons.shaizi4 ;
-    }else if(va == '5'){
-      icon = AppIcons.shaizi5 ;
-    }else if(va == '6'){
-      icon = AppIcons.shaizi6 ;
+  }
+
+  static Container _k3(_str, int index) {
+    var icon = AppIcons.shaizi1;
+    var va = _str[index];
+    if (va == '2') {
+      icon = AppIcons.shaizi2;
+    } else if (va == '3') {
+      icon = AppIcons.shaizi3;
+    } else if (va == '4') {
+      icon = AppIcons.shaizi4;
+    } else if (va == '5') {
+      icon = AppIcons.shaizi5;
+    } else if (va == '6') {
+      icon = AppIcons.shaizi6;
     }
     return new Container(
       /*constraints: BoxConstraints(
@@ -104,8 +102,8 @@ if (gameEn.contains( 'k3')) {
       ),*/
 //      padding: EdgeInsets.all(10.0),
       child: new FittedBox(
-          fit: BoxFit.fill,
-          child: Icon(icon, color: Colors.red),
+        fit: BoxFit.fill,
+        child: Icon(icon, color: Colors.red),
       ),
     );
   }
