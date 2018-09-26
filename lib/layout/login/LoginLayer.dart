18202c04fa7831f38bf5b9264083937ea912e2e2
xingdu <caixingdu@vipyoung.org>
2018-09-26 17:09:23 +0800

    登陆注册调整

--- lib/layout/login/LoginLayer.dart
+++ lib/layout/login/LoginLayer.dart
@@ -4,6 +4,7 @@ import 'package:lowlottery/font/index.dart';
 import 'package:flutter/services.dart';
 import 'package:lowlottery/store/appStore.dart';
 import 'package:lowlottery/layout/register/RegisterLayer.dart';
+import 'package:lowlottery/font/index.dart';
 class LoginLayer extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -26,106 +27,104 @@ class _LoginPageState extends State<LoginLayer> {
     return new Scaffold(
       appBar: new AppBar(
         centerTitle: true,
-        automaticallyImplyLeading: false, //禁止pop
+        automaticallyImplyLeading: false,
+        //禁止pop
         title: new Text("登录", style: new TextStyle(color: Colors.white)),
         iconTheme: new IconThemeData(color: Colors.white),
-          actions: <Widget>[
-            new OutlineButton(
-              borderSide:new BorderSide(color: Theme.of(context).primaryColor),
-              child: new Text('立即注册',style: new TextStyle(color: Colors.white),),
-              onPressed: (){
-                Navigator.of(context).push(new MaterialPageRoute(
-                  builder: (context) => new RegisterLayer(),
-                ));
-              },
-            )
-
-          ],
-      ),
-      body: new Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-
-          new Container(
-            // constraints: new BoxConstraints(maxHeight: 150.0),
-            child: new Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                new Padding(
-                  padding: new EdgeInsets.fromLTRB(
-                      leftRightPadding, 50.0, leftRightPadding, 10.0),
-//              child: new Image.asset(LOGO),
-                ),
-                new Padding(
-                  padding: new EdgeInsets.fromLTRB(leftRightPadding, 50.0,
-                      leftRightPadding, topBottomPadding),
-                  child: new TextFormField(
-                    autofocus: true,
-                    keyboardType: TextInputType.text,
-                    style: hintTips,
-                    maxLines: 1,
-                    controller: _userNameController,
-                    textInputAction: TextInputAction.next,
-                    decoration: new InputDecoration(
-                        hintText: "注册的手机号或者邮箱",
-                        border: new UnderlineInputBorder(),
-                        icon: Icon(Icons.local_phone)),
-                    //obscureText: true,
-                  ),
-                ),
-                new Padding(
-                  padding: new EdgeInsets.fromLTRB(leftRightPadding, 30.0,
-                      leftRightPadding, topBottomPadding),
-                  child: new TextField(
-                    keyboardType: TextInputType.text,
-                    style: hintTips,
-                    controller: _userPassController,
-                    maxLines: 1,
-                    textInputAction: TextInputAction.done,
-                    decoration: new InputDecoration(
-                      icon: Icon(AppIcons.passwd),
-                      hintText: "登陆密码",
-                      border: new UnderlineInputBorder(),
-                    ),
-                    obscureText: true,
-                  ),
-                ),
-                new Container(
-                  width: 360.0,
-                  margin: new EdgeInsets.fromLTRB(10.0, 40.0, 10.0, 0.0),
-                  padding: new EdgeInsets.fromLTRB(leftRightPadding,
-                      topBottomPadding, leftRightPadding, topBottomPadding),
-                  child: new Card(
-                    color: Colors.red,
-                    elevation: 6.0,
-                    child: new FlatButton(
-                      onPressed: () {
-                        print("the username is" + _userNameController.text);
-                        print("the pass is" + _userPassController.text);
-                        StoreProvider.of<AppState>(context).dispatch(
-                          new LoginRequestAction(
-                            context,
-                            {
-                              "username": _userNameController.text,
-                              "pwd": _userPassController.text
-                            },
-                          ),
-                        );
-                      },
-                      child: new Text(
-                        '马上登录',
-                        style:
-                            new TextStyle(color: Colors.white, fontSize: 16.0),
-                      ),
-                    ),
-                  ),
-                )
-              ],
+        actions: <Widget>[
+          new OutlineButton(
+            borderSide: new BorderSide(color: Theme.of(context).primaryColor),
+            child: new Text(
+              '立即注册',
+              style: new TextStyle(color: Colors.white),
             ),
-          ),
+            onPressed: () {
+              Navigator.of(context).push(new MaterialPageRoute(
+                builder: (context) => new RegisterLayer(),
+              ));
+            },
+          )
         ],
+      ),
+      body: new SingleChildScrollView(
+//        fit: StackFit.expand,
+        child: new Container(
+          // constraints: new BoxConstraints(maxHeight: 150.0),
+          child: new Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              new Padding(
+                padding: new EdgeInsets.fromLTRB(
+                    leftRightPadding, 50.0, leftRightPadding, 10.0),
+//              child: new Image.asset(LOGO),
+              ),
+              new Padding(
+                padding: new EdgeInsets.fromLTRB(
+                    leftRightPadding, 50.0, leftRightPadding, topBottomPadding),
+                child: new TextFormField(
+                  keyboardType: TextInputType.text,
+                  style: hintTips,
+                  maxLines: 1,
+                  controller: _userNameController,
+                  textInputAction: TextInputAction.next,
+                  decoration: new InputDecoration(
+                      hintText: "注册的手机号或者邮箱",
+                      border: new UnderlineInputBorder(),
+                      icon: Icon(AppIcons.account)),
+                  //obscureText: true,
+                ),
+              ),
+              new Padding(
+                padding: new EdgeInsets.fromLTRB(
+                    leftRightPadding, 30.0, leftRightPadding, topBottomPadding),
+                child: new TextField(
+                  keyboardType: TextInputType.text,
+                  style: hintTips,
+                  controller: _userPassController,
+                  maxLines: 1,
+                  textInputAction: TextInputAction.done,
+                  decoration: new InputDecoration(
+                    icon: Icon(AppIcons.passwd),
+                    hintText: "登陆密码",
+                    border: new UnderlineInputBorder(),
+                  ),
+                  obscureText: true,
+                ),
+              ),
+              new Container(
+                width: 360.0,
+                margin: new EdgeInsets.fromLTRB(10.0, 40.0, 10.0, 0.0),
+                padding: new EdgeInsets.fromLTRB(leftRightPadding,
+                    topBottomPadding, leftRightPadding, topBottomPadding),
+                child: new Card(
+                  color: Colors.red,
+                  elevation: 6.0,
+                  child: new FlatButton(
+                    onPressed: () {
+                      print("the username is" + _userNameController.text);
+                      print("the pass is" + _userPassController.text);
+                      StoreProvider.of<AppState>(context).dispatch(
+                        new LoginRequestAction(
+                          context,
+                          {
+                            "username": _userNameController.text,
+                            "pwd": _userPassController.text
+                          },
+                        ),
+                      );
+                    },
+                    child: new Text(
+                      '马上登录',
+                      style: new TextStyle(color: Colors.white, fontSize: 16.0),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/layout/register/RegisterLayer.dart
+++ lib/layout/register/RegisterLayer.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:lowlottery/font/index.dart';
 import 'package:lowlottery/store/appStore.dart';
-
+import 'package:lowlottery/font/index.dart';
 class RegisterLayer extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -31,87 +31,81 @@ class _LoginPageState extends State<RegisterLayer> {
     return new Scaffold(
       appBar: new AppBar(
         centerTitle: true,
-        title: new Text("注册", style: new TextStyle(color: Colors.white)),
+        title: new Text("账号注册", style: new TextStyle(color: Colors.white)),
         iconTheme: new IconThemeData(color: Colors.white),
       ),
-      body: new Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-
-          new Container(
-            child: new Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                new Padding(
-                  padding: new EdgeInsets.fromLTRB(
-                      leftRightPadding, 50.0, leftRightPadding, 10.0),
+      body: new SingleChildScrollView(
+        child: new Container(
+          child: new Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              new Padding(
+                padding: new EdgeInsets.fromLTRB(
+                    leftRightPadding, 50.0, leftRightPadding, 10.0),
 //              child: new Image.asset(LOGO),
+              ),
+              new Padding(
+                padding: new EdgeInsets.fromLTRB(
+                    leftRightPadding, 50.0, leftRightPadding, topBottomPadding),
+                child: new TextField(
+                  style: hintTips,
+                  maxLines: 1,
+                  keyboardType: TextInputType.text,
+                  controller: _userNameController,
+                  textInputAction: TextInputAction.next,
+                  decoration: new InputDecoration(
+                    hintText: "手机号或者邮箱",
+                    border: new UnderlineInputBorder(),
+                    icon: Icon(AppIcons.account),
+                  ),
+                  // obscureText: true,
                 ),
-                new Padding(
-                  padding: new EdgeInsets.fromLTRB(leftRightPadding, 50.0,
-                      leftRightPadding, topBottomPadding),
-                  child: new TextField(
-                    autofocus: true,
-                    style: hintTips,
-                    maxLines: 1,
-                    keyboardType: TextInputType.text,
-                    controller: _userNameController,
-                    textInputAction: TextInputAction.next,
-                    decoration: new InputDecoration(
-                      hintText: "手机号或者邮箱",
+              ),
+              new Padding(
+                padding: new EdgeInsets.fromLTRB(
+                    leftRightPadding, 30.0, leftRightPadding, topBottomPadding),
+                child: new TextField(
+                  style: hintTips,
+                  controller: _userPassController,
+                  textInputAction: TextInputAction.done,
+                  decoration: new InputDecoration(
+                      hintText: "设置6-12位密码",
                       border: new UnderlineInputBorder(),
-                      icon: Icon(Icons.local_phone),
-                    ),
-                    // obscureText: true,
-                  ),
+                      icon: Icon(AppIcons.passwd)),
+                  obscureText: true,
+                  maxLines: 1,
                 ),
-                new Padding(
-                  padding: new EdgeInsets.fromLTRB(leftRightPadding, 30.0,
-                      leftRightPadding, topBottomPadding),
-                  child: new TextField(
-                    style: hintTips,
-                    controller: _userPassController,
-                    textInputAction: TextInputAction.done,
-                    decoration: new InputDecoration(
-                        hintText: "设置6-12位密码",
-                        border: new UnderlineInputBorder(),
-                        icon: Icon(AppIcons.passwd)),
-                    obscureText: true,
-                    maxLines: 1,
-                  ),
-                ),
-                new Container(
-                  width: 360.0,
-                  margin: new EdgeInsets.fromLTRB(10.0, 40.0, 10.0, 0.0),
-                  padding: new EdgeInsets.fromLTRB(leftRightPadding,
-                      topBottomPadding, leftRightPadding, topBottomPadding),
-                  child: new Card(
-                    color: Colors.red,
-                    elevation: 6.0,
-                    child: new FlatButton(
-                      onPressed: () {
-                        print("the username is" + _userNameController.text);
-                        print("the pass is" + _userPassController.text);
-                        StoreProvider.of<AppState>(context).dispatch(
-                            new RegisterRequestAction(context, {
-                          "username": _userNameController.text,
-                          "password": _userPassController.text
-                        }));
-                      },
-                      child: new Text(
-                        '立即注册',
-                        style:
-                            new TextStyle(color: Colors.white, fontSize: 16.0),
-                      ),
+              ),
+              new Container(
+                width: 360.0,
+                margin: new EdgeInsets.fromLTRB(10.0, 40.0, 10.0, 0.0),
+                padding: new EdgeInsets.fromLTRB(leftRightPadding,
+                    topBottomPadding, leftRightPadding, topBottomPadding),
+                child: new Card(
+                  color: Colors.red,
+                  elevation: 6.0,
+                  child: new FlatButton(
+                    onPressed: () {
+                      print("the username is" + _userNameController.text);
+                      print("the pass is" + _userPassController.text);
+                      StoreProvider.of<AppState>(context).dispatch(
+                          new RegisterRequestAction(context, {
+                        "username": _userNameController.text,
+                        "password": _userPassController.text
+                      }));
+                    },
+                    child: new Text(
+                      '立即注册',
+                      style: new TextStyle(color: Colors.white, fontSize: 16.0),
                     ),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
